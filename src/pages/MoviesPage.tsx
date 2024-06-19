3b28fd2b84b28ace7b33a89e7a8fedc8b0052ac3
Valakas148 <ilyabychkov5x404@gmail.com>
2024-06-19 19:11:58 +0300

    4%

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useAppDispatch} from "../redux/store";
+import {movieAction} from "../redux/slices/MovieSlice";
+import MoviesComponent from "../components/MoviesComponents/MoviesComponent";
 
 const MoviesPage = () => {
+
+    let dispatch=useAppDispatch()
+
+    useEffect(() => {
+        dispatch(movieAction.loadMovies())
+    }, []);
+
     return (
         <div>
-            
+            <MoviesComponent/>
         </div>
     );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {useDispatch, useSelector} from "react-redux";
 
 export const store = configureStore({
     reducer: {}
 })
+
+export const useAppDispatch = useDispatch.withTypes<typeof store.dispatch>()
+export const useAppSelector = useSelector.withTypes<ReturnType<typeof store.getState>>
