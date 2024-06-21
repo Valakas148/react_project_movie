44d3975752c3398ce0cc622f0e09e4680725e069
Valakas148 <ilyabychkov5x404@gmail.com>
2024-06-22 01:58:54 +0300

    TOOOO FIIINIIISH

--- src/components/SearchMoviesComponents/SearchMoviesComponent.tsx
+++ src/components/SearchMoviesComponents/SearchMoviesComponent.tsx
@@ -22,8 +22,12 @@ const SearchMoviesComponent:FC<IProps> = ({onSubmit}) => {
     return (
         <div className={styles.SearchComponent}>
             <form onSubmit={handleSubmit(foundMovie)} className={styles.SearchForm}>
-                <input type="text" {...register('searchWord')} placeholder="Type name of movie..." className={styles.InputSearch}/>
-                <button type="submit" className={styles.ButtonSearch}>Search</button>
+                <input type="text" {...register('searchWord')} placeholder="Type name of movie..."
+                       className={styles.InputSearch}/>
+                <button type="submit" className={styles.ButtonSearch}>
+                    <img src="https://img.icons8.com/ios-glyphs/30/search--v1.png" alt="search"
+                         className={styles.SearchIcon}/>
+                </button>
             </form>
         </div>
     );
